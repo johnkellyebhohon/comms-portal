a021afe84d82954f969c384a2c075a87c7670791
johnkelly ebhohon <johnkellyworld0@gmail.com>
2020-04-17 03:57:53 +0100

    added another navbar to the right

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+.bd - placeholder - img {
+    font - size: 1.125 rem;
+    text - anchor: middle; -
+    webkit - user - select: none; -
+    moz - user - select: none; -
+    ms - user - select: none;
+    user - select: none;
+}
+
+@media(min - width: 768 px) {
+    .bd - placeholder - img - lg {
+        font - size: 3.5 rem;
+    }
+}
